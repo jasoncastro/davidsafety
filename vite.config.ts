f076591598df4bc294c6ec3d4a0b624dfecac225
Jason Christopher Castro <jasoncastro15@gmail.com>
2026-06-10 00:58:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/davidsafety/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
